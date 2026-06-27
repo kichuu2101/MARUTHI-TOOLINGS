153d7625d40876118375e8cf8f81d05a406cea62
kichuu2101 <kk6895586@gmail.com>
2026-06-27 21:03:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,5 +22,54 @@ window.addEventListener("scroll",()=>{
         }
 
     });
+    /* Counter Animation */
+
+const counters = document.querySelectorAll(".counter");
+
+const startCounter = () => {
+    counters.forEach(counter => {
+
+        const target = +counter.getAttribute("data-target");
+
+        let count = 0;
+
+        const updateCounter = () => {
+
+            const increment = target / 100;
+
+            if(count < target){
+                count += increment;
+                counter.innerText = Math.ceil(count) + "+";
+                setTimeout(updateCounter,20);
+            }else{
+                counter.innerText = target + "+";
+            }
+        };
+
+        updateCounter();
+    });
+};
+
+startCounter();
+
+
+/* Scroll Reveal Animation */
+
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+
+    reveals.forEach(reveal => {
+
+        const windowHeight = window.innerHeight;
+        const revealTop = reveal.getBoundingClientRect().top;
+
+        if(revealTop < windowHeight - 100){
+            reveal.style.opacity = "1";
+            reveal.style.transform = "translateY(0)";
+        }
+    });
+
+});
 
 });
